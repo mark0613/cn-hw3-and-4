c46d4c8b3844f652050fb247ad7d096ff5c3d6ab
mark0613 <61940915+mark0613@users.noreply.github.com>
2025-04-19 05:19:26 +0000

    feat: docker 部署

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8888',
+        target: 'http://backend:8888',
         changeOrigin: true
       }
     }
